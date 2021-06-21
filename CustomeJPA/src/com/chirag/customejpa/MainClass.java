986ae64c65bea5a3fd808af3ca3691b2aa52eaa3
chiragj <chirag.j@ezdi.us>
2021-06-21 11:59:48 +0530

    minor commit

--- CustomeJPA/src/com/chirag/customejpa/MainClass.java
+++ CustomeJPA/src/com/chirag/customejpa/MainClass.java
@@ -14,7 +14,6 @@ public class MainClass {
 	
 	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
 		
-		
 	}
 	
 	
